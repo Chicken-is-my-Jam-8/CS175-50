5a482e9cb490f4a7ee71b44bac056f68a7c045df
Chicken-is-my-Jam-8 <s1347556@monmouth.edu>
2024-10-18 14:39:00 -0400

    Add files via upload

---
+++ myInfo.py
@@ -0,0 +1,6 @@
+
+# CS175-50
+# Alexander J. Jackson
+# I live in [blank], in the city of [blank], in the state of [blank], & my zip code is [blank]. My telephone number is [blank.
+
+# Major: Computer Science
